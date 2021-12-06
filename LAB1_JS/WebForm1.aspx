8eb24fa34dbb0d0c656baf16e6f653b7d1725dde
Roxor0 <Roxor0@DESKTOP-RHBJIG5>
2021-12-06 15:06:16 +0100

    Dodaj pliki projektów.

---
+++ LAB1_JS/WebForm1.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LAB1_JS.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+<style type="text/css">
+
+.container {
+  width: 80%;
+  height: 200px;
+  margin: auto;
+  padding: 10px;
+}
+
+.one {
+  width: 30%;
+  height: 1000px;
+  
+  float: left;
+}
+
+.two {
+  margin-left: 15%;
+  position: relative;
+  height: 500px;
+
+
+}
+
+</style>
+</head>
+<body style="height: 1000px">
+   
+    
+    <div class="container">
+            <div class="one">
+                 <form id="form1" runat="server">
+            Okreśłenie rozmiaru elementów
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem Value="s">Mały rozmiar elementu</asp:ListItem>
+                <asp:ListItem Value="m">Średni rozmiar elementu</asp:ListItem>
+                <asp:ListItem Value="x">Duży rozmiar elementu</asp:ListItem>
+            </asp:RadioButtonList>
+   
+            Okreslenie polozenia rysunków
+            <br />
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+                <asp:ListItem Value="0">Kostka piątka</asp:ListItem>
+                <asp:ListItem Value="1">Przekątna lewa</asp:ListItem>
+                <asp:ListItem Value="2">Przekątna prawa</asp:ListItem>
+                <asp:ListItem Value="3">Linia poziomo</asp:ListItem>
+                <asp:ListItem Value="4">Linia pionowo</asp:ListItem>
+            </asp:DropDownList>
+             </form>
+            </div>
+            <div class ="two">
+                <asp:Image ID="Image0" runat="server" src="/and.jpeg" width="50" height="50"></asp:Image>
+                <asp:Image ID="Image1" runat="server" src="/and.jpeg" width="50" height="50"></asp:Image>
+                <asp:Image ID="Image2" runat="server" src="/and.jpeg" width="50" height="50"></asp:Image>
+                <asp:Image ID="Image3" runat="server" src="/and.jpeg" width="50" height="50"></asp:Image>
+                <asp:Image ID="Image4" runat="server" src="/and.jpeg" Width="50" Height="50"></asp:Image>
+            </div>   
+    </div>
+    
+   
+     
+    </body>
+</html>
---
+++ LAB1_JS/WebForm1.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace LAB1_JS
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+
+        /// <summary>
+        /// Kontrolka DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Kontrolka Image0.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image0;
+
+        /// <summary>
+        /// Kontrolka Image1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
+        /// Kontrolka Image2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
+        /// Kontrolka Image3.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
+
+        /// <summary>
+        /// Kontrolka Image4.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image4;
+    }
+}
